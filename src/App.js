c6e1448e016fff78ff92e91a04875f8b8b135c0c
Chloe <chloezhouny@gmail.com>
2019-09-12 14:36:14 -0700

    Add navbar and router link

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './components/Navbar';
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import Services from './pages/Services'
+import About from './pages/About'
+
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function App() {
+class App extends React.Component {
+
+  render() {
   return (
     <Router>
       <div>
+      <h2>hello</h2>
+      <Nav/>
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route exact path='/services' component={Services} />
@@ -18,6 +27,7 @@ function App() {
       </div>
     </Router>
   );
+}
 }
 
 export default App;
