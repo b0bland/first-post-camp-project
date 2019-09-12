a06ceca8e3940f6226dbacc5b05f196e2be9dfb8
Chloe <chloezhouny@gmail.com>
2019-09-12 15:16:07 -0700

    change active

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -29,7 +29,7 @@ return(
 	        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
 	      </li>
 	     
-	      <li className="nav-item dropdown active">
+	      <li className="nav-item dropdown">
 	        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 	          Services
 	        </a>
@@ -40,7 +40,7 @@ return(
 	          <a className="dropdown-item" href="#">Drain Cleaning</a>
 	        </div>
 	      </li>
-	       <li className="nav-item active">
+	       <li className="nav-item">
 	        <a className="nav-link" href="#">About Us</a>
 	      </li>
 	    </ul>
