289f9a9d95829d13041c8e874f498a44a1da2f2f
b0bland <alfredychan96@gmail.com>
2019-09-14 02:44:00 -0700

    Added basic polygon info

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -3,15 +3,30 @@ import './style.css';
 import { Map, GoogleApiWrapper, Polygon} from 'google-maps-react';
 
 class Maps extends Component {
-
-
     render () {
+        const polyCoords = [
+            { lat: 37.811078, lon: -122.477195},
+            { lat: , lon: },
+            { lat: , lon: },
+            { lat: , lon: },
+            { lat: , lon: },
+            { lat: , lon: },
+            { lat: , lon: },
+            { lat: , lon: }
+        ];
+
         return (
             <Map 
             google={this.props.google} 
             zoom={9} 
             initialCenter={{ lat: 37.703028, lon: -122.393602}} >
-
+                <Polygon 
+                    paths={polyCoords}
+                    strokeColor="#ff1500"
+                    strokeOpacity={0.8}
+                    strokeWeight={2}
+                    fillColor="#ff1500"
+                    fillOpacity={0.35} />
             </Map>
         )
     }
